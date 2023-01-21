b2305a04904be637c3138aa8fb05532b9e417a43
kryne001 <kyler.rynear@gmail.com>
2023-01-20 16:10:59 -0800

    added subsets

---
+++ subsets_recursion/src/App.java
@@ -0,0 +1,35 @@
+import java.util.*;;
+
+public class App {
+    
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(nums);
+        backtrack(list, new ArrayList<>(), nums, 0);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
+        list.add(new ArrayList<>(tempList));
+        for(int i = start; i < nums.length; i++){
+             tempList.add(nums[i]);
+            backtrack(list, tempList, nums, i + 1);
+            tempList.remove(tempList.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        int nums[] = {1,2,3};
+        App myApp = new App();
+        List<List<Integer>> answer = myApp.subsets(nums);
+        
+        for (List<Integer> c : answer)
+        {
+            System.out.print("[");
+            for (int i = 0; i < c.size(); i++) System.out.print(c.get(i) + ", ");
+            System.out.print("] ");
+        }
+
+        
+    }
+}
